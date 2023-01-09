a781fcef6a7cc0ea2cb6a88aa72c6470d40e2169
sonalighodake43772 <sonalighodake867>
2023-01-09 18:14:14 +0530

    create portal

--- src/components/Layout/HeaderCart.js
+++ src/components/Layout/HeaderCart.js
@@ -5,12 +5,12 @@ import  classes from './HeaderCart.module.css';
 const HeaderCart=()=>
 {
     return(
-        <button className={classes['button']} >
+        <button className={classes.button} >
  <span className={classes.icon}>
    <CartIcon/>
    </span>
 <h3 className={classes['cart-title']}>Your Cart </h3>
-<h3 className={classes['badge']}> 0</h3>
+<h3 className={classes.badge}> 0</h3>
 
         </button>
 
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,33 @@
+import React,{Fragment} from 'react';
+import  ReactDOM  from 'react-dom';
+import classes from './Modal.module.css';
+
+  const BackDrop=(props)=>
+    {
+return <div className={classes.backdrop}/>
+    }
+
+    const Modalverlays=(props)=>
+    {
+return (
+<div className={classes.modal}>
+    <div className={classes.content}>{props.children}</div>
+</div>
+);
+
+ }
+ const portalElement=document.getElementById('overlays');
+ const Modal=(props)=>
+{
+  
+    return(
+
+        <Fragment>
+            
+            {ReactDOM.createPortal(<BackDrop/>,portalElement)}
+            {ReactDOM.createPortal(<Modalverlays>{props.children}</Modalverlays>,portalElement)}
+        </Fragment>
+    )
+
+}
+export default Modal;
