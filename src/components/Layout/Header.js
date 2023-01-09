8d6cb318015b2a27c6a73587abd0b4503738fe1e
sonalighodake43772 <sonalighodake867>
2023-01-09 18:12:56 +0530

    change css

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,26 +1,20 @@
 import React, { Fragment } from "react";
 import HeaderCart from "./HeaderCart";
-import  classes from './Header.module.css';
-import meals from '../../images/meals.jpg';
+import classes from "./Header.module.css";
+import meals from "../../images/meals.jpg";
 
-const Header=()=>
-{
-    
-    return(
-        <Fragment>
-        <header className={classes['header']}>
-            <h1>ReactMeals</h1>
-         
-<HeaderCart/>
-</header>
-<div className={classes['main-image']}>
-        <img src={meals} alt='A table full of delicious food!' />
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+
+        <HeaderCart />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={meals} alt="A table full of delicious food!" />
       </div>
-
-        
-        </Fragment>
-
-
-    );
-}
+    </Fragment>
+  );
+};
 export default Header;
