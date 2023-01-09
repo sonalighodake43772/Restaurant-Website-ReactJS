99c3b3ce5e2599bf1046d7ea45d9a38429493b50
sonalighodake43772 <sonalighodake867>
2023-01-09 18:11:03 +0530

    add Cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React,{Fragment} from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
-    <div>
+    <Fragment>
+      <Cart/>
      <Header/>
      < Meals/>
-    </div>
+    </Fragment>
   );
 }
 
