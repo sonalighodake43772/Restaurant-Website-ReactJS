8a3f308a60be55b07f54c86219597ab9a053f622
sonalighodake43772 <sonalighodake867>
2023-01-10 12:41:38 +0530

    add showcart pointer

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,13 +3,13 @@ import HeaderCart from "./HeaderCart";
 import classes from "./Header.module.css";
 import meals from "../../images/meals.jpg";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
 
-        <HeaderCart />
+        <HeaderCart onClick={props.onShowCart}/>
       </header>
       <div className={classes["main-image"]}>
         <img src={meals} alt="A table full of delicious food!" />
