c7680912910f6d06f643b40e7eb64f63fe955ca7
sonalighodake43772 <sonalighodake867>
2023-01-10 12:40:52 +0530

    show and close cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React,{Fragment} from "react";
+import React,{Fragment,useState} from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartShow,setCartIsShow]=useState(false);
+
+  const ShowCartHandler=()=>
+  {
+    setCartIsShow(true);
+  }
+  const HideCartHandler=()=>
+  {
+    setCartIsShow(false);
+  }
+
+
+
   return (
     <Fragment>
-      <Cart/>
-     <Header/>
+      {cartShow && <Cart onHideCart={HideCartHandler}/>}
+     <Header onShowCart={ShowCartHandler}/>
      < Meals/>
     </Fragment>
   );
