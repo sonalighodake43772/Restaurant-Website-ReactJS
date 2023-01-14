bcaad5ca4905ec5df4114d6f74885f84c0c5358c
sonalighodake43772 <sonalighodake867>
2023-01-14 13:35:12 +0530

    add context and implemement  additemtocart

--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,27 +1,36 @@
-import React  from "react";
-import classes from './MealItemForm.module.css';
+import React, { useContext } from "react";
+import classes from "./MealItemForm.module.css";
 import Input from "../UI/Input";
+import CartContext from "../../store/Cart-Context";
 
-const MealItemForm=(props)=>
-{
-    return(
-        
-        <form className={classes.form}>
+const MealItemForm = (props) => {
+  const cartcntx = useContext(CartContext);
+  const additemtocart = (event) => {
+    event.preventDefault();
+    const quantity=document.getElementById("amount_" + props.id).value;
+  
+    cartcntx.addItem({...props.item,quantity:quantity});
+  
+
+
+    console.log("after additem cart", cartcntx);
+  };
+  return (
+    <form className={classes.form}>
+      {console.log('inside render',cartcntx.items)}
       <Input
-        label='Amount'
+        label="Amount"
         input={{
-          id: 'amount_' + props.id,
-          type: 'number',
-          min: '1',
-          max: '5',
-          step: '1',
-          defaultValue: '1',
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
         }}
       />
-      <button>+ Add</button>
+      <button onClick={additemtocart}>+ Add</button>
     </form>
-
-    );
-
-}
+  );
+};
 export default MealItemForm;
