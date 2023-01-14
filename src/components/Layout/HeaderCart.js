88405cd0f4a4cddad90640a7495eae6a17b5b539
sonalighodake43772 <sonalighodake867>
2023-01-14 13:33:51 +0530

    add cartcontext and calculate cart quantity

--- src/components/Layout/HeaderCart.js
+++ src/components/Layout/HeaderCart.js
@@ -1,19 +1,24 @@
-import React from "react";
-import CartIcon from '../Cart/CartIcon';
-import  classes from './HeaderCart.module.css';
+import React, { useContext } from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCart.module.css";
+import CartContext from "../../store/Cart-Context";
 
-const HeaderCart=(props)=>
-{
-    return(
-        <button className={classes.button} onClick={props.onClick} >
- <span className={classes.icon}>
-   <CartIcon/>
-   </span>
-<h3 className={classes['cart-title']}>Your Cart </h3>
-<h3 className={classes.badge}> 0</h3>
+const HeaderCart = (props) => {
+  const cartctx = useContext(CartContext);
+  let quantity = 0;
 
-        </button>
+  cartctx.items.forEach((item) => {
+    quantity = quantity + Number(item.quantity);
+  });
 
-    );
-}
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span className={classes["cart-title"]}>Your Cart </span>
+      <span className={classes.badge}>{quantity}</span>
+    </button>
+  );
+};
 export default HeaderCart;
