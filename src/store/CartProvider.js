b6de3be1d6487805e91e79a7aac629b777fdef51
sonalighodake43772 <sonalighodake867>
2023-01-17 10:36:49 +0530

    implement additemcart and removecart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -11,23 +11,39 @@ const CartProvider = (props) => {
     if (index === -1) {
       items.push({
         ...item,
-        quantity: 1,
+        quantity: Number(item.quantity),
       });
       const updateCart = [...items];
       updateItems(updateCart);
     } else {
-      items[index].quantity += 1;
-      items[index].price = items[index].price * items[index].quantity;
+      items[index].quantity = items[index].quantity + Number(item.quantity);
+
+      //  items[index].price = items[index].price * items[index].quantity;
       const updateCart = [...items];
       updateItems(updateCart);
     }
   };
-  const RemoveItemFromCart = (id) => {};
+  const RemoveItemFromCart = (id) => {
+    const exist = items.find((x) => x.id === id);
+    if (exist.quantity === 1) {
+      updateItems(items.filter((x) => x.id !== id));
+    } else {
+      updateItems(
+        items.map((x) =>
+          x.id === id
+            ? {
+                ...exist,
+                quantity: exist.quantity - 1,
+              }
+            : x
+        )
+      );
+    }
+  };
 
   const cartContext = {
     items: items,
-
-    addItem: AddItemToCart,
+   addItem: AddItemToCart,
     removeItem: RemoveItemFromCart,
   };
 
